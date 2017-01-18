b9bf73e8c55667479d704275900f48e020dc656d
Mario <hammer.mario@web.de>
2017-01-18 16:30:46 +0100

    fixed actions to return initial values

--- src/main/java/bbm/actions/impl/MonitorAction.java
+++ src/main/java/bbm/actions/impl/MonitorAction.java
@@ -3,6 +3,9 @@ package bbm.actions.impl;
 import bbm.actions.Action;
 import bbm.actions.ActionResult;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mario on 1/18/17.
  */
@@ -12,6 +15,16 @@ public class MonitorAction implements Action {
 
     @Override
     public ActionResult execute() {
-        return null;
+        return new ActionResult() {
+            @Override
+            public Boolean getSuccess() {
+                return true;
+            }
+
+            @Override
+            public Map<String, String> getPayload() {
+                return new HashMap<>();
+            }
+        };
     }
 }
