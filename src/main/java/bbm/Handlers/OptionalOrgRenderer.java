95be7416992758f013136a825b9200f3590dcc9f
Mario <hammer.mario@web.de>
2017-01-19 11:58:58 +0100

    changed optional renderer to be generic

--- src/main/java/bbm/Handlers/OptionalOrgRenderer.java
+++ src/main/java/bbm/Handlers/OptionalOrgRenderer.java
@@ -11,13 +11,13 @@ import java.util.Optional;
 /**
  * Created by mario on 1/19/17.
  */
-public class OptionalOrgRenderer extends RendererSupport<Optional<Org>> {
+public class OptionalOrgRenderer extends RendererSupport<Optional<?>> {
     @Override
-    public void render(Context ctx, Optional<Org> org) throws Exception {
+    public void render(Context ctx, Optional<?> org) throws Exception {
         JsonObject result = new JsonObject();
-        if(org.isPresent()) {
+        if(org.isPresent() && org.get().getClass() == Org.class) {
             result.addProperty("success", true);
-            result.addProperty("orgName", org.get().getName());
+            result.addProperty("orgName", ((Org)org.get()).getName());
         } else {
             result.addProperty("success", false);
         }
